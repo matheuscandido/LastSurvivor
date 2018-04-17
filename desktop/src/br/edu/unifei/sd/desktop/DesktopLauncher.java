f38654612f2fc6d9ce7f299b726c4039e2079c92
Matheus Candido <matheusoc@live.com>
2018-04-17 08:21:35 -0300

    First commit

---
+++ desktop/src/br/edu/unifei/sd/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package br.edu.unifei.sd.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import br.edu.unifei.sd.LastDotSurvivorGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LastDotSurvivorGame(), config);
+	}
+}
